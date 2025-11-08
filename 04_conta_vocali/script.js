99ddbbad1e09a33b5cbf2bf144c632392504d7b1
morad <KARKARMORAD29@GMAIL.COM>
2025-11-08 10:25:46 +0100

    fatto il 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,14 +1,38 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
+function countVowels(str) {
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countVowels(word));
 
 
 
 // Dichiara la funzione qui.
+function countVowels(str) {
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
 
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(countVowels(word));
 
 
 
